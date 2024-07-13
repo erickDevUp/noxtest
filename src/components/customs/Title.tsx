b9010cda0ffc1b5a125859c000e107966f299d50
erickdevup <erickdevup@gmail.com>
2024-07-13 09:28:02 +0200

    custom title section

---
+++ src/components/customs/Title.tsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react'
+import React from 'react'
+
+type Props = {text: string}
+
+export default function Title({text}: Props) {
+  return (
+    <Heading as='h2' size={"2xl"} fontWeight={"bold"} color={"nox.text"} textAlign={"center"}>
+      {text}
+    </Heading>
+  )
+}
