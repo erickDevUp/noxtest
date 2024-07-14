c6e74535518827768396aaba84c334614bc75bf5
erickdevup <erickdevup@gmail.com>
2024-07-14 16:13:19 +0200

    subtitle custom fix

--- src/components/customs/Subtitle.tsx
+++ src/components/customs/Subtitle.tsx
@@ -1,11 +1,11 @@
 import { Text } from '@chakra-ui/react'
 import React from 'react'
 
-type Props = {text: string}
+type Props = {text: string, textAlign?: "center" | "start" | "end"}
 
-export default function Subtitle({text}: Props) {
+export default function Subtitle({text,textAlign="center"}: Props) {
   return (
-    <Text fontSize={"lg"} fontWeight={"normal"} color={"nox.text"} align={"center"}>
+    <Text fontSize={"lg"} fontWeight={"normal"} color={"nox.text"} align={textAlign}>
       {text}
     </Text>
   )
