9f0db3bc44c0afa480c53c22037b792ba7c652f7
erickdevup <erickdevup@gmail.com>
2024-07-14 16:13:02 +0200

    fix Green Btn name

---
+++ src/components/customs/GreenButtom.tsx
@@ -0,0 +1,14 @@
+
+import { Button } from '@chakra-ui/react'
+import Link from 'next/link'
+import React from 'react'
+
+type Props = { text: string, link: string }
+
+export default function GreenButtom({ text ,link}: Props) {
+  return (
+    <Button as={Link} href={link} bg={"nox.primary"} p={6} fontWeight={"semibold"} _hover={{ bg: "green.300" }} _active={{ bg: "green.300" }} color={"white"} fontSize={"md"} borderRadius={"full"}>
+      {text}
+    </Button>
+  )
+}
