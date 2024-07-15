5d0e6d60162394cd7c3a9f95488e24fd4f2d745a
erickdevup <erickdevup@gmail.com>
2024-07-15 22:29:10 +0200

    subtitle props changed

--- src/components/howWeWork/Card.tsx
+++ src/components/howWeWork/Card.tsx
@@ -1,4 +1,4 @@
-import { Heading, Stack } from '@chakra-ui/react'
+import {Heading, Stack } from '@chakra-ui/react'
 import Image from 'next/image'
 import React from 'react'
 import Subtitle from '../customs/Subtitle'
@@ -16,12 +16,13 @@ type Props = {
 export default function Card({ img, text, title, btnText, btnLink, imgPosition }: Props) {
     return (
         <Stack align={"center"} direction={['column',imgPosition === "left" ? "row" : "row-reverse"]}>
+           
             <Image src={img} alt="card" width={607} height={551} />
             <Stack align={"start"} spacing={8} textAlign={"start"} >
                 <Heading as='h3' pt={1} size={"lg"} fontWeight={"medium"} color={"nox.text"} >
                     {title}
                 </Heading>
-                <Subtitle text={text} textAlign={"start"} />
+                <Subtitle text={text} />
                 <GreenButtom text={btnText} link={btnLink} />
             </Stack>
         </Stack>
