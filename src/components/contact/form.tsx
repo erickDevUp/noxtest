ef907a4d34ef46229830c8b63950dbd8f928324c
erickdevup <erickdevup@gmail.com>
2024-07-15 23:40:46 +0200

    fix btn form direction

--- src/components/contact/form.tsx
+++ src/components/contact/form.tsx
@@ -3,20 +3,20 @@ import React from 'react'
 import GreenButtom from '../customs/GreenButtom'
 
 export default function Form() {
-    return (
-        <FormControl>
-        <Stack spacing={5} justify={"space-between"} maxWidth="container">
-          <Stack direction={{ base: "column", md: "row" }} spacing={5} justify={"space-between"}>
-            <Input variant='outline' placeholder='Nombre' focusBorderColor='nox.primary' />
-            <Input variant='outline' placeholder='Apellido' focusBorderColor='nox.primary' />
-          </Stack>
-          <Input variant='outline' placeholder='Correo' type='email' focusBorderColor='nox.primary' />
-          <Input variant='outline' placeholder='Telefono' type='tel' focusBorderColor='nox.primary' />
-          <Textarea variant='outline' placeholder='Escriba en un breve texto' focusBorderColor='nox.primary' />
-          <Box alignSelf={{ base: "self-end", md: "flex-start" }}>
-            <GreenButtom text='Enviar' link='#' />
-          </Box>
+  return (
+    <FormControl>
+      <Stack spacing={5} justify={"space-between"} maxWidth="container">
+        <Stack direction={{ base: "column", md: "row" }} spacing={5} justify={"space-between"}>
+          <Input variant='outline' placeholder='Nombre' focusBorderColor='nox.primary' />
+          <Input variant='outline' placeholder='Apellido' focusBorderColor='nox.primary' />
         </Stack>
-      </FormControl>
-    )
+        <Input variant='outline' placeholder='Correo' type='email' focusBorderColor='nox.primary' />
+        <Input variant='outline' placeholder='Telefono' type='tel' focusBorderColor='nox.primary' />
+        <Textarea variant='outline' placeholder='Escriba en un breve texto' focusBorderColor='nox.primary' />
+        <Box alignSelf={"end"}>
+          <GreenButtom text='Enviar' link='#' />
+        </Box>
+      </Stack>
+    </FormControl>
+  )
 }
