ff57b3f1d99849db166142cc2f5bf108528be294
erickdevup <erickdevup@gmail.com>
2024-07-16 00:01:08 +0200

    main home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 
-import { Text } from "@chakra-ui/react";
+import Contact from "@/components/homePage/contact";
+import Hero from "@/components/homePage/hero";
+import HowWeWork from "@/components/homePage/howWeWork";
+import Services from "@/components/homePage/services";
+import Team from "@/components/homePage/team";
+import Works from "@/components/homePage/works";
+import { Box, Stack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Text color={"nox.primary"} fontSize={"2xl"}>Config</Text>
+    <Box>
+      <Stack paddingX={[7,0]} maxW={1220} margin={"auto"} spacing={[32,48]} my={[32,48]}>
+        <Hero />
+        <Services />
+        <HowWeWork />
+        <Team />
+        <Works />
+      </Stack>
+      <Contact />
+
+    </Box>
   );
 }
