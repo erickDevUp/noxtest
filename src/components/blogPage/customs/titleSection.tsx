e3c9b1bacf13fe266f5332acea68baf2f37630f9
erickdevup <erickdevup@gmail.com>
2024-07-16 20:03:49 +0200

    custom title section component

---
+++ src/components/blogPage/customs/titleSection.tsx
@@ -0,0 +1,10 @@
+import { Heading } from '@chakra-ui/react'
+import React from 'react'
+
+type Props = { title: string }
+
+export default function TitleSection({ title }: Props) {
+    return (
+        <Heading as="h2" alignSelf={"start"} fontSize={"2xl"} fontWeight={"semibold"} color="nox.text-blog">{title}</Heading>
+    )
+}
