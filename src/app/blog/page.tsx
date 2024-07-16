50b1c5c1b4d97b84634e4596be9d1def264f2396
erickdevup <erickdevup@gmail.com>
2024-07-16 20:05:04 +0200

    blog page done

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,12 @@
+import AllPosts from "@/components/blogPage/allPosts";
+import FeaturedPosts from "@/components/blogPage/featuredPosts";
+import { Stack } from "@chakra-ui/react";
 
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <Stack px={[7,0]} maxWidth={["full",1220]} overflow={["hidden","visible"]} w={"full"} justify={"center"} align={"center"}margin={"auto"} spacing={10} my={[10,16]}>
+      <FeaturedPosts />
+      <AllPosts />
+    </Stack>
   )
 }
