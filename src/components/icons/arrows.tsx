10127b789b2c8582ee906a92e493a153d8ffbd4b
erickdevup <erickdevup@gmail.com>
2024-07-16 20:04:22 +0200

    add arrows icons

---
+++ src/components/icons/arrows.tsx
@@ -0,0 +1,18 @@
+
+export function BackArrow() {
+    return (
+    <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12.8333 7.49996H1.16663M1.16663 7.49996L6.99996 13.3333M1.16663 7.49996L6.99996 1.66663" stroke="#7B7B7B" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+
+    )
+}
+export function NextArrow() {
+    return (
+        <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.16675 7.49996H12.8334M12.8334 7.49996L7.00008 1.66663M12.8334 7.49996L7.00008 13.3333" stroke="#7B7B7B" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        
+
+    )
+}
