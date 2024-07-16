67f6606eb575456565f9aeffe6a67708e1cf681e
erickdevup <erickdevup@gmail.com>
2024-07-16 20:03:32 +0200

    all blogs section done

---
+++ src/components/blogPage/allPosts/data.ts
@@ -0,0 +1,84 @@
+
+export const Posts = [
+    {
+      img: "/img/blog/post4.png",
+      date: "2023-04-01",
+      author: "Alec Whitten",
+      title: "Introducción a React",
+      slug:"introduccion-a-react",
+      description: "En esta publicación exploraremos los fundamentos de React y cómo comenzar con tu primer proyecto.",
+      tags: ["React", "Desarrollo Web", "Tutorial"]
+    },
+    {
+      img: "/img/blog/post5.png",
+      date: "2023-03-15",
+      author: "Alec Whitten",
+      title: "Primeros pasos en Node.js",
+      slug:"primeros-pasos-en-node-js",
+      description: "Un tutorial completo sobre cómo configurar y empezar a desarrollar aplicaciones con Node.js.",
+      tags: ["Node.js", "Back-end", "Tutorial"]
+    },
+    {
+      img: "/img/blog/post6.png",
+      date: "2023-02-28",
+      author: "Alec Whitten",
+      title: "Mastering CSS Grid",
+      slug:"mastering-css-grid",
+      description: "Profundizamos en las capacidades de CSS Grid para crear diseños responsivos y flexibles.",
+      tags: ["CSS", "Grid", "Diseño"]
+    },
+    {
+      img: "/img/blog/post7.png",
+      date: "2023-02-10",
+      author: "Alec Whitten",
+      title: "Guía completa de Git",
+      slug:"guia-completa-de-git",
+      description: "Aprende desde cero hasta avanzado sobre cómo gestionar tu código con Git.",
+      tags: ["Git", "Control de Versiones", "Tutorial"]
+    },
+    {
+      img: "/img/blog/post8.png",
+      date: "2023-01-25",
+      author: "Alec Whitten",
+      title: "Integración continua con Jenkins",
+      slug:"integracion-continua-con-jenkins",
+      description: "Exploramos cómo automatizar tus flujos de trabajo de desarrollo con Jenkins.",
+      tags: ["Jenkins", "CI/CD", "Automatización"]
+    },
+    {
+      img: "/img/blog/post9.png",
+      date: "2023-01-01",
+      author: "Alec Whitten",
+      title: "Principios de diseño UX/UI",
+      slug:"principios-de-diseno-ux-ui",
+      description: "Descubre los fundamentos del diseño de experiencias de usuario y de interfaces de usuario.",
+      tags: ["UX", "UI", "Diseño"]
+    },
+    {
+      img: "/img/blog/post4.png",
+      date: "2022-12-15",
+      author: "Alec Whitten",
+      title: "Desarrollo de APIs RESTful",
+      slug:"desarrollo-de-apis-restful",
+      description: "Aprende a diseñar y construir APIs RESTful eficientes y escalables.",
+      tags: ["APIs", "REST", "Back-end"]
+    },
+    {
+      img: "/img/blog/post7.png",
+      date: "2022-11-30",
+      author: "Alec Whitten",
+      title: "Testing en JavaScript",
+      slug:"testing-en-javascript",
+      description: "Explora técnicas y herramientas para probar tu código JavaScript de manera efectiva.",
+      tags: ["JavaScript", "Testing", "QA"]
+    },
+    {
+      img: "/img/blog/post9.png",
+      date: "2022-11-15",
+      author: "Alec Whitten",
+      title: "Introducción a TypeScript",
+      slug:"introduccion-a-typescript",
+      description: "Comienza a aprovechar las ventajas de TypeScript en tus proyectos de JavaScript.",
+      tags: ["TypeScript", "JavaScript", "Tipado Estricto"]
+    },
+  ];
---
+++ src/components/blogPage/allPosts/index.tsx
@@ -0,0 +1,34 @@
+"use client"
+import React from 'react'
+import { Posts } from './data'
+import CardPost from '../customs/cardPost'
+import { Stack } from '@chakra-ui/react'
+import { useSearchParams } from 'next/navigation';
+import Pagination from './pagination'
+import TitleSection from '../customs/titleSection'
+
+export default function AllPosts() {
+    const postsPerPage = 6;
+    const page = useSearchParams().get("page")
+
+    const pageStr: any = page ?? '1';
+    let pageNumber = Number.isNaN(parseInt(pageStr)) ? 1 : parseInt(pageStr);
+
+    const currentPosts = Posts.slice((pageNumber - 1) * postsPerPage, pageNumber * postsPerPage);
+    const numberPages = Math.ceil(Posts.length / postsPerPage)
+
+
+
+    return (
+        <Stack direction={"column"}>
+            <TitleSection title='Todos los post' />
+            <Stack mt={5} direction={["column", "row"]} wrap={["nowrap", "wrap"]} maxW={["full", "fit-content"]} spacing={8} justify={"center"}>
+                {currentPosts.map((post) => (
+                    <CardPost slug={post.slug} author={post.author} date={post.date} title={post.title} description={post.description} tags={post.tags} img={post.img} key={post.slug} />
+                ))}
+            </Stack>
+            <Pagination numberPages={numberPages} pageNumber={pageNumber} />
+
+        </Stack>
+    )
+}
