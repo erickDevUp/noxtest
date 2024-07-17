3d5407066dd36e208fa319078303edfdd946aff2
erickdevup <erickdevup@gmail.com>
2024-07-17 07:41:25 +0200

    fix nav router

--- src/components/layout/nav/data.ts
+++ src/components/layout/nav/data.ts
@@ -6,11 +6,11 @@ interface Route {
 export type NavRoutes = Array<Route>;
 // src/components/layout/navRoutes.ts
 export const navRoutes: NavRoutes = [
-  { id: 1, label: "Servicios", href: "#Servicios" },
-  { id: 2, label: "Pasos", href: "#Pasos" },
-  { id: 3, label: "Nuestro Equipo", href: "#Nuestro-Equipo" },
-  { id: 4, label: "Trabajos", href: "#Trabajos" },
-  { id: 5, label: "Comentarios", href: "#Comentarios" },
-  { id: 6, label: "Contacto", href: "#Contacto" },
+  { id: 1, label: "Servicios", href: "/#Servicios" },
+  { id: 2, label: "Pasos", href: "/#Pasos" },
+  { id: 3, label: "Nuestro Equipo", href: "/#Nuestro-Equipo" },
+  { id: 4, label: "Trabajos", href: "/#Trabajos" },
+  { id: 5, label: "Comentarios", href: "/#Comentarios" },
+  { id: 6, label: "Contacto", href: "/#Contacto" },
   { id: 7, label: "Blog", href: "/blog" },
 ];
